96c65ae18b529417ff37a68822e1da82fba13f48
KittDoesntCode <50064397+KittDoesntCode@users.noreply.github.com>
2025-04-07 11:45:19 -0400

    Update http2.go

--- cmd/pmoxs3backuproxy/http2.go
+++ cmd/pmoxs3backuproxy/http2.go
@@ -5,8 +5,8 @@ import (
 	"encoding/hex"
 	"net"
 	"time"
-	"tizbac/pmoxs3backuproxy/internal/s3backuplog"
-	"tizbac/pmoxs3backuproxy/internal/s3pmoxcommon"
+	"KittDoesntCode/pmoxs3backuproxy/internal/s3backuplog"
+	"KittDoesntCode/pmoxs3backuproxy/internal/s3pmoxcommon"
 
 	"github.com/juju/clock"
 	"github.com/juju/mutex/v2"
